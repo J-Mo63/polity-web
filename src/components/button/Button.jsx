d92eea8194d42a84fa060173b5ece9faaa9c76e8
Jonathan Moallem <jdmoallem@gmail.com>
2018-07-25 10:10:33 +1000

    Added button component

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './button.css';
+
+const Button = ({ text, colour, size }) => (
+    <button className="button"
+         style={{
+             background: colour,
+             borderColor: colour,
+             fontSize: size
+         }}>{ text }</button>
+);
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+    colour: PropTypes.string,
+    size: PropTypes.string
+};
+
+export default Button
